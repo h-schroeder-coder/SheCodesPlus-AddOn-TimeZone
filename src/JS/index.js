6522bb0f074d4b84dff85eaa393267b951d1eb65
h-schroeder-coder <hankorschroeder@gmail.com>
2024-12-28 09:39:15 -0600

    Injected JS for showDetails function

--- src/JS/index.js
+++ src/JS/index.js
@@ -1,3 +1,17 @@
-alert("It is current day, current MMMM D, YYYY HH:mm A in selected country/city");
+function showDetails(event) {
+  if (event.target.value.length > 0) {
+    let currentTime = moment()
+      .tz(event.target.value)
+      .format("dddd, MMMM D, YYYY HH:mm A");
 
-let currentDateDetails = 
+        let resultsCurrentDetailsElement = document.querySelector(
+      "#results-current-details"
+    );
+    resultsCurrentDetailsElement.innerHTML = `It is ${currentTime} in ${event.target.value}`;
+
+    alert(`It is ${currentTime} in ${event.target.value}`);
+  }
+}
+
+let selectedCityElement = document.querySelector("#city");
+selectedCityElement.addEventListener("change", showDetails);
